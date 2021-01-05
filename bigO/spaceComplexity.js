9bf5d87ae140aca50fff513898f6ab12f7242693
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-06 00:53:33 +0100

    Space Complexity

--- bigO/spaceComplexity.js
+++ bigO/spaceComplexity.js
@@ -10,7 +10,7 @@ booooo([1,2,3,4,5]);
 //Time Complexity: O(n)
 //Space Complexity: O(1)
 
-function arrayofHiNtimes {
+function arrayofHiNtimes(n) {
     let hiArray = [];
     for (let i=0; i<n; i++) {
         hiArray[i] = "Hi";
