059089a98931740dcf353d3bbb2c0b08e52f8983
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-06 00:14:22 +0100

    Quadratic Time

---
+++ bigO/quadraticTime.js
@@ -0,0 +1,15 @@
+// Log all pairs of the indices of the array
+
+const boxes = ["a","b","c","d","e"];
+
+function logAllPairsOfArray(array) {
+    for (let i=0; i < boxes.length; i++) {
+        for (let j=0; j < boxes.length; j++) {
+            console.log(array[i], array[j]);
+        }
+    }
+}
+
+logAllPairsOfArray(boxes);.
+
+//BigO: O(n ^ 2)
