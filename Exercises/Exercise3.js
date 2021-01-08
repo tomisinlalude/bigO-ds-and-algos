ae2af8b0578dc541068d1d1b775663fff6bd6261
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-08 21:42:57 +0100

    Add another exercise

---
+++ Exercises/Exercise3.js
@@ -0,0 +1,34 @@
+/*
+Analytic skills: How can you think through problems?
+Coding skills: Do you write clean, high performance codes?
+Technical knowledge: 
+Communication skills:
+*/
+/*
+Step by step through a problem:
+1. When the interviewer says the question, write down the
+2. Make sure you double check: What are the inputs?
+3. What is the most important value of the problem?
+4. Don't be annoying and ask too many questions
+5. Start with the naive/brute force approach
+6. Tell them why this approach is not the best (i.e)
+7. Walk through your approach, comment things and see
+8. Before you start coding, walk through your code
+9. Modularize your code from the very beginning.
+10. Start actually writing your code now. Keep in mind
+11. Think abour error checks and how you can break this
+12.Don't use bad conflicting names like i and j
+13. Test your code: Check for no params, 0, undefined,
+14. Finally, talk to the interviewer where you would 
+15.If your interviewer is happy with the solution,
+*/
+/*
+Good code checklist
+- It works
+- Good use of data structures
+- Code re-use/Do not repeat yourself
+- Modular, Makes code more readable, maintainable
+- Less than O(n^2), we want to avoid nested loops
+- Low space complexity, recursion can cause stack 
+*/
+
