47411327b9c5e3ff6b5842ca49cccf00d6a69546
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-09 17:09:39 +0100

    Better solution

--- Exercises/Exercise3.js
+++ Exercises/Exercise3.js
@@ -83,9 +83,20 @@ const array2 = ['x', 'y', 'z']
 
 function containsCommonItem2(arr1, arr2) {
     //loop through first array and create object where properties === items in the array
-    
+    let map = {};
+    for (let i=0; i < arr1.length; i++) {
+        if(!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
     //loop through second array and check if item in second array exists on created account
-    
+    for (let j=0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 
