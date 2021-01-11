bf30912c548616c68b7d9a07baf0ac1707808756
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-11 18:08:34 +0100

    Arrays - Reverse a string

--- Exercises/Exercise4.js
+++ Exercises/Exercise4.js
@@ -2,7 +2,7 @@
 //Reverse a string
 
 function reverse1(str) {
-    if (!str && str.length < 2 && typeof str === 'string') {
+    if (!str || str.length < 2 || typeof str === 'string') {
         console.log("Your input is not acceptable.")
     }
     for (let i = str.length-1; i >= 0; i--) {
