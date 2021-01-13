e422321aacc9e5e1fb4946b54c13aecb525a25bd
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-13 22:08:33 +0100

    Merged Sorted Array

---
+++ dataStructures/Arrays/Exercise5.js
@@ -0,0 +1,13 @@
+//Merged Sort Array
+
+function mergeSortedArray(array1, array2) {
+    const mergedArray = [];
+    let arrayItem = array1[0];
+    let array2Item = array2[0];
+
+    //Check input
+
+    return mergedArray;
+}
+
+mergeSortedArray([0,3,4,31], [4,6,30]);
