f15f48989d3f2a563d6d3491bbf575f1da179045
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-14 21:24:42 +0100

    Create a Hash table

--- dataStructures/HashTables/Exercise6.js
+++ dataStructures/HashTables/Exercise6.js
@@ -1,2 +1,19 @@
 //Create a hash table
 
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+      // this.data = [];
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+
+  }
+  
+  const myHashTable = new HashTable(50);
