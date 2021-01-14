1d6788a2c1a9c2b9bc7db58734ecac06c2e31828
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-14 21:25:27 +0100

    Set a key-value pair in a hash table

--- dataStructures/HashTables/Exercise6.js
+++ dataStructures/HashTables/Exercise6.js
@@ -13,7 +13,17 @@ class HashTable {
       }
       return hash;
     }
-
+  
+    set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
   }
   
   const myHashTable = new HashTable(50);
+  myHashTable.set('grapes', 10000)
+  myHashTable.set('apples', 9)
