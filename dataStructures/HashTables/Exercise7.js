41fcf5fcd47f20e77d939dedafa2fadaa267b471
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-14 21:32:46 +0100

    Brute force method - First recurring character

--- dataStructures/HashTables/Exercise7.js
+++ dataStructures/HashTables/Exercise7.js
@@ -1,4 +1,3 @@
-
 //First recurring character
 /*
 Google Question
@@ -11,3 +10,17 @@ It should return 1
 Given an array = [2,3,4,5]:
 It should return undefined
 */
+
+//Brute force method
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+}
+
+firstRecurringCharacter([1,5,5,1,3,4,6]);
