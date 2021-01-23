5eeafe01d420b3b1fc7fc551bbd67fa0c5065ad4
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-23 16:56:57 +0100

    Find maximum subarray

--- dataStructures/Arrays/Assignment4.js
+++ dataStructures/Arrays/Assignment4.js
@@ -8,3 +8,35 @@ Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
  */
+
+ /*
+ PSEUDOCODE
+1. Assign your variables using the worst case scenario i.e
+Sum being negative infinity, first index of subarray being first index of original array
+then last index of subarray being last index of original array
+2. Loop through each element of the original array in nested loops
+3. In the first loop, assign the sum of the subarray to zero
+3. In the innermost loop, compare the initial sum, negative infinity to the sum of the subarray
+*/
+
+var maxSubArray = (nums) => {
+    var max_so_far = Number.NEGATIVE_INFINITY;
+    var len = nums.length;
+
+    for (var i = 0; i < len; i++) {
+
+        for (var j = i; j < len; j++) {
+            maxSum = 0;
+            for (var k = i; k <= j; k++) {
+                maxSum += nums[k];
+
+                if (max_so_far < maxSum) {
+                    leftIndex = i;
+                    max_so_far = maxSum;
+                    rightIndex = j;
+                }
+            }
+        }
+    }
+    return max_so_far;
+}
