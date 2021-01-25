2324b355879564cb4458d4d2de7995c908066d19
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-25 09:33:02 +0100

    MaxSubArray bruteforce solution

--- dataStructures/Arrays/Assignment4.js
+++ dataStructures/Arrays/Assignment4.js
@@ -45,3 +45,29 @@ var maxSubArray = (nums) => {
 }
 
 //Quadratic Algorithm
+
+function findMaxSubArrayBruteForce(arr) {
+	var max_so_far = Number.NEGATIVE_INFINITY;
+	var leftIndex = 0,
+		rightIndex = arr.length - 1,
+		len = arr.length;
+
+	for (var i = 0; i < len; i++) {
+		maxSum = 0;
+
+		for (var j = i; j < len; j++) {
+			maxSum += arr[j];
+
+			if (max_so_far < maxSum) {
+				leftIndex = i;
+				max_so_far = maxSum;
+				rightIndex = j;
+			}
+		}
+	}
+	return {
+		left: leftIndex,
+		right: rightIndex,
+		final_max_sum_subArray: max_so_far
+	};
+}
