973ea2bc866a18bcc43b047ec3cbd3633cbb1eb8
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-27 00:01:21 +0100

    Kadane Algo

--- dataStructures/Arrays/Assignment4.js
+++ dataStructures/Arrays/Assignment4.js
@@ -120,23 +120,35 @@ mx = max( s_l, s_r, t, sub_right.mx, sub_left.mx, sub_left.r+sub_right.l)
     max_ending_here = 0
   */
 
- var maxSequence = function(arr){
+  var maxSequence = function(arr){
 
-	var curr_max = 0, max_so_far = 0;
-  
-	for(var i = 0; i < arr.length; i++){  
-	  curr_max = Math.max(0, curr_max + arr[i]);
-	  max_so_far = Math.max(curr_max, max_so_far);
-	}
-	return max_so_far;
+  var curr_max = 0, max_so_far = 0;
+
+  for(var i = 0; i < arr.length; i++){  
+    curr_max = Math.max(0, curr_max + arr[i]);
+    max_so_far = Math.max(curr_max, max_so_far);
   }
-  
-  console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // returns 6 : [4, -1, 2, 1]
-  
-  /* If the solution specifically requires, that if the list is made up of only negative numbers or an empty list is given, zero should be returned - then few more lines can be added to explicitly take care of that. */
-  
-  let allPositives = arr => arr.every(n => n > 0)
-  let allNegatives = arr => arr.every(n => n < 0)
-  let sum = arr => arr.reduce((curr_max, max_so_far) => curr_max + max_so_far, 0)
-  
-  
+  return max_so_far;
+}
+
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // returns 6 : [4, -1, 2, 1]
+
+/* If the solution specifically requires, that if the list is made up of only negative numbers or an empty list is given, zero should be returned - then few more lines can be added to explicitly take care of that. */
+
+let allPositives = arr => arr.every(n => n > 0)
+let allNegatives = arr => arr.every(n => n < 0)
+let sum = arr => arr.reduce((curr_max, max_so_far) => curr_max + max_so_far, 0)
+
+var maxSequence = function(arr){
+  if(arr.length === 0 || allNegatives(arr)) return 0;
+  if(allPositives(arr)) return sum(arr);
+
+  var curr_max = 0, max_so_far = 0;
+
+  for(var i = 0; i < arr.length; i++){  
+    curr_max = Math.max(0, curr_max + arr[i]);
+    max_so_far = Math.max(curr_max, max_so_far);
+  }
+  return max_so_far;
+}
+
