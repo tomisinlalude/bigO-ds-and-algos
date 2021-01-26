3e8bb21aa6c73f0e1c634587a587e5cec5ffc352
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-26 23:59:14 +0100

    Kadane Algo

--- dataStructures/Arrays/Assignment4.js
+++ dataStructures/Arrays/Assignment4.js
@@ -108,3 +108,31 @@ mx = max( s_l, s_r, t, sub_right.mx, sub_left.mx, sub_left.r+sub_right.l)
 *  If function “Max_Opposite” is O(n²), then T(n) = O(n²). But if we manage to make it O(n), then
 *  T(n) = O(nlogn)
  */
+
+
+
+// Kadane's ALgo
+ /**
+  * We can easily solve this problem in linear time using Kadane’s;s algorithm. The idea is to maintain maximum (positive sum) sub-array ending at each index of the given array. This subarray is either empty (in which case its sum is zero) or consists of one more element than the maximum subarray ending at the previous index.
+    The time complexity of above solution is **O(n)** and auxiliary space used by the program is **O(1)**
+    Simple idea of the Kadane’s algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). And keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). Each time we get a positive sum compare it with max_so_far and update max_so_far if it is greater than max_so_far
+    Initialize: max_so_far = 0
+    max_ending_here = 0
+  */
+
+ var maxSequence = function(arr){
+
+	var curr_max = 0, max_so_far = 0;
+  
+	for(var i = 0; i < arr.length; i++){  
+	  curr_max = Math.max(0, curr_max + arr[i]);
+	  max_so_far = Math.max(curr_max, max_so_far);
+	}
+	return max_so_far;
+  }
+  
+  console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // returns 6 : [4, -1, 2, 1]
+  
+  /* If the solution specifically requires, that if the list is made up of only negative numbers or an empty list is given, zero should be returned - then few more lines can be added to explicitly take care of that. */
+  
+  
