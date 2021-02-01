8799763010e9fbfeba40dee5b24285a8bd38d242
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2021-02-01 20:33:59 +0100

    Two pointers

---
+++ dataStructures/Arrays/Assignment5.js
@@ -0,0 +1,10 @@
+/**
+*Given a sorted array A (sorted in ascending order), 
+*having N integers, find if there exists any pair of elements 
+*(A[i], A[j]) such that their sum is equal to X.
+ */
+
+//Naive solution
+
+
+//Two pointers Technique
